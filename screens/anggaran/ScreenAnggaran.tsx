14d6caaa7b030520aa5d94fa9ed9d27e69ce1072
mbadrudin170 <mbadrudin170@gmail.com>
2026-02-20 22:22:47 +0000

    Changes before Firebase Studio auto-run

---
+++ screens/anggaran/ScreenAnggaran.tsx
@@ -0,0 +1,10 @@
+import { Pressable, Text, View } from 'react-native';
+export default function ScreenAnggaran() {
+  return (
+    <View>
+      <Pressable>
+        <Text>kategori</Text>
+      </Pressable>
+    </View>
+  );
+}
